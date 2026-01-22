de93e7b37ec39190a4f74268e8aba2a50f35cbd6
AyurSaanchezL <ayur.sanchez@educa.madrid.org>
2026-01-22 11:27:05 +0100

    ya podéis compilar!!

---
+++ compile.bat
@@ -0,0 +1,10 @@
+@echo off
+echo ===============================
+echo   Compilando el proyecto
+echo ===============================
+call mvnw compile
+echo.
+echo ===============================
+echo   Compilación completada
+echo ===============================
+pause
---
+++ run.bat
@@ -0,0 +1,10 @@
+@echo off
+echo ===============================
+echo   Ejecutando JavaFX con Maven
+echo ===============================
+call mvnw javafx:run
+echo.
+echo ===============================
+echo   Programa finalizado
+echo ===============================
+pause
